d78a37f1510abcbaf27c173abaf5640870245ee4
irfan ardhiyanto <irfanardhiyanto@gmail.com>
2018-05-26 23:35:02 +0700

    Update PostsFromPageExtractor.java

--- PostsFromPageExtractor.java
+++ PostsFromPageExtractor.java
@@ -13,9 +13,9 @@ public class PostsFromPageExtractor {
 /**
  * A simple Facebook4J client which
  * illustrates how to access group feeds / posts / comments.
- * 
+ *
  * @param args
- * @throws FacebookException 
+ * @throws FacebookException
  */
 public static void main(String[] args) throws FacebookException {
 
@@ -23,10 +23,10 @@ public static void main(String[] args) throws FacebookException {
     Facebook facebook = new FacebookFactory().getInstance();
     // Use default values for oauth app id.
     facebook.setOAuthAppId("178054789703690", "4cbfc033984d871bbfe42e19295b7f72");
-    // Get an access token from: 
+    // Get an access token from:
     // https://developers.facebook.com/tools/explorer
     // Copy and paste it below.
-    String accessTokenString = "PASTE_YOUR_ACCESS_TOKEN_HERE";
+    String accessTokenString = "EAACEdEose0cBAFQZB1Gc76eu6lZBNegJgKRmZAUe9OmA7EBXXQ5lb7HjEUjpL6UYfxMlaGDS5pZApBNAOrZAyTdZBu4iBru2B97MEBTmxR7ulaWaesSXYUDAYz0mahCMUSdqTimFsE1xgvuZAFg8AtPiA4JYcBnZA8tLyhIBh66q46kKWZBqMNDCSqG8K6cOWYNHjwZBIwIw61nQZDZD";
     AccessToken at = new AccessToken(accessTokenString);
     // Set access token.
     facebook.setOAuthAccessToken(at);
@@ -37,9 +37,8 @@ public static void main(String[] args) throws FacebookException {
     // https://developers.facebook.com/tools/explorer
 
     // Set limit to 25 feeds.
-    ResponseList<Post> feeds = facebook.getFeed("187446750783",
+   /* ResponseList<Post> feeds = facebook.getFeed("187446750783",
             new Reading().limit(25));
-
         // For all 25 feeds...
         for (int i = 0; i < feeds.size(); i++) {
             // Get post.
@@ -48,12 +47,13 @@ public static void main(String[] args) throws FacebookException {
             String message = post.getMessage();
                             // Print out the message.
             System.out.println(message);
-
             // Get more stuff...
             PagableList<Comment> comments = post.getComments();
             String date = post.getCreatedTime().toString();
             String name = post.getFrom().getName();
             String id = post.getId();
-        }           
+        }*/
+        facebook.postStatusMessage("Hello");
+
     }
 }
